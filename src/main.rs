8d92ecabe522c5987d4fd643521361098c6edcc5
Daniel <danielac@protonmail.com>
2023-08-04 07:04:34 -0600

    Update count_to_100 to count_to_100_million

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 fn main() {
-    count_to_100();
+    count_to_100_million();
+
 }
 
-fn count_to_100() {
-    for i in 1..=100 {
-        print!("{}, ", i);
-        if (i % 10) == 0 {
-            println!()
+fn count_to_100_million() {
+    let total_i32 = 100000000;
+    let total_string = format!("{}", total_i32);
+    println!("Counting to {}...", total_string);
+    for i in 1..=total_i32 {
+        if i == total_i32 {
+            print!("Finished counting to {}", total_string);
         }
     }
 }
