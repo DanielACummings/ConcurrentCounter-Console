548cf14e311d05cb72f6d97c4a8c303b22b81e5d
Daniel <danielac@protonmail.com>
2023-08-04 17:43:40 -0600

    Add double_from_1 function & make shared variables used by all counting functions

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 fn main() {
-    count_to_100_million();
-
+    let count_total = 1_000_000_000;
+    let count_total_string = format!("{}", count_total);
+    count_by_1(&count_total, &count_total_string);
+    double_from_1(&count_total, &count_total_string);
 }
 
-fn count_to_100_million() {
-    let total_i32 = 100000000;
-    let total_string = format!("{}", total_i32);
-    println!("Counting to {}...", total_string);
-    for i in 1..=total_i32 {
-        if i == total_i32 {
-            print!("Finished counting to {}", total_string);
+fn count_by_1(&count_total: &i32, count_total_string: &String) {
+    println!("Counting to {}...", count_total_string);
+
+    for i in 1..=count_total {
+        if i % 100_000_000 == 0 {
+            println!("{} ", i);
+        }
+        if i == count_total {
+            println!("Finished counting");
         }
     }
+}
+
+fn double_from_1(&count_total: &i32, count_total_string: &String) {
+    println!("\nDoubling from 1 the closed doubled number >= {}...", count_total_string);
+    let mut i = 1;
+ 
+    while i < count_total {
+        i *= 2;
+        println!("{} ", i);
+    }
+    print!("Finished counting");
 }
