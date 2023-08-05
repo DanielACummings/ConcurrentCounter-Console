e968c2eaedfcbfbed3400048cec551e66434ddc9
Daniel <danielac@protonmail.com>
2023-08-05 17:07:08 -0600

    Add count_by_fibonacci_nums function

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,54 @@ fn main() {
     let count_total_string = format!("{}", count_total);
     count_by_1(&count_total, &count_total_string);
     double_from_1(&count_total, &count_total_string);
+    count_by_fibonacci_nums(&count_total, &count_total_string);
 }
 
 fn count_by_1(&count_total: &i32, count_total_string: &String) {
-    println!("Counting to {}...", count_total_string);
-
+    println!("Counting by 1 to {}:", count_total_string);
     for i in 1..=count_total {
         if i % 100_000_000 == 0 {
             println!("{} ", i);
         }
-        if i == count_total {
-            println!("Finished counting");
+    }
+}
+
+fn count_by_fibonacci_nums(
+    &count_total: &i32,
+    count_total_string: &String
+) {
+    let mut i: i32 = 1;
+    let mut current: i32 = 1;
+    let mut prev: i32 = 0;
+    
+    println!("\nCounting using fibonacci numbers from 1 to \
+        the closest number >= {}:", {count_total_string});
+    loop {
+        // Print "prev" then compare to "count_total" before continuing to avoid
+        // unnecessary processing
+        println!("{}: {}", (i).to_string(), prev);
+        if prev > count_total {
+            break;
         }
+
+        let next: i32 = prev + current;
+
+        prev = current;
+        current = next;
+        i += 1;
     }
 }
 
 fn double_from_1(&count_total: &i32, count_total_string: &String) {
-    println!("\nDoubling from 1 the closed doubled number >= {}...", count_total_string);
     let mut i = 1;
- 
-    while i < count_total {
-        i *= 2;
+
+    println!("\nDoubling from 1 to the closest number >= {}:", count_total_string);
+    loop {
         println!("{} ", i);
+
+        if i > count_total {
+            break;
+        }
+        i *= 2;
     }
-    print!("Finished counting");
 }
