caae6956b579ee9daa6eeec815e3b6d77f3f587d
Daniel <danielac@protonmail.com>
2023-08-09 19:05:25 -0600

    Display time taken to run all counting functions consecutively

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
+use std::time::Instant;
+use std::iter::repeat;
+
 fn main() {
+    // TODO: add commas to printed numbers 
     let count_total = 1_000_000_000;
     let count_total_string = format!("{}", count_total);
+    let indent: String = repeat(" ").take(4).collect();
     let functions = vec!(count_by_1, count_by_fibonacci_nums, double_from_1);
 
+    // Run counting functions consecutively
+    let start_time = Instant::now();
     for function in functions {
         function(&count_total, &count_total_string);
     }
+    let elapsed_time = start_time.elapsed().as_secs();
+
+    // Display results
+    println!(
+"\nTotal time taken to run all counting functions:
+{0}Consecutively: {1}
+{0}Concurrently: _ seconds (future feature)",
+        indent, elapsed_time
+    );
 }
 
 fn count_by_1(&count_total: &i32, count_total_string: &String) {
