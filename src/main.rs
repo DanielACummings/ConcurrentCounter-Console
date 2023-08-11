23d1efe52c1e6ca12837df05f8c867ce7b7689e4
Daniel <danielac@protonmail.com>
2023-08-10 18:50:04 -0600

    Replace consecutively run functions with an Tokyo async function call

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,36 @@
-use std::time::Instant;
 use std::iter::repeat;
+use std::time::Instant;
+use tokio::task::spawn;
+use tokio::runtime::Runtime;
 
 fn main() {
     // TODO: add commas to printed numbers 
     let count_total = 1_000_000_000;
     let count_total_string = format!("{}", count_total);
     let indent: String = repeat(" ").take(4).collect();
-    let functions = vec!(count_by_1, count_by_fibonacci_nums, double_from_1);
-
-    // Run counting functions consecutively
+    
+    // Run counting functions concurrently
     let start_time = Instant::now();
-    for function in functions {
-        function(&count_total, &count_total_string);
-    }
+    
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async {
+        let task = spawn(async move {count_by_1(&count_total, &count_total_string).await;
+    });
+        task.await.unwrap();
+    });
+
     let elapsed_time = start_time.elapsed().as_secs();
 
     // Display results
     println!(
 "\nTotal time taken to run all counting functions:
-{0}Consecutively: {1}
-{0}Concurrently: _ seconds (future feature)",
+{0}Consecutively: (future feature)
+{0}Concurrently: {1} seconds",
         indent, elapsed_time
     );
 }
 
-fn count_by_1(&count_total: &i32, count_total_string: &String) {
+async fn count_by_1(&count_total: &i32, count_total_string: &String) {
     println!("Counting by 1 to {}:", count_total_string);
     for i in 1..=count_total {
         // Print progress every time another 10% of the total is counted
@@ -35,7 +41,7 @@ fn count_by_1(&count_total: &i32, count_total_string: &String) {
     }
 }
 
-fn count_by_fibonacci_nums(
+async fn count_by_fibonacci_nums(
     &count_total: &i32,
     count_total_string: &String
 ) {
@@ -61,7 +67,7 @@ fn count_by_fibonacci_nums(
     }
 }
 
-fn double_from_1(&count_total: &i32, count_total_string: &String) {
+async fn double_from_1(&count_total: &i32, count_total_string: &String) {
     let mut i = 1;
 
     println!("\nDoubling from 1 to the closest number >= {}:", count_total_string);
