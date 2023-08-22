5ad10d7f8ace34842c87cf596af83959a7d1f2aa
Daniel <danielac@protonmail.com>
2023-08-21 21:00:16 -0600

    Run same functions concurrently then consecutively

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,89 @@
 use std::iter::repeat;
 use std::time::Instant;
-use tokio::task::spawn;
+use tokio::join;
 use tokio::runtime::Runtime;
 
 fn main() {
     // TODO: add commas to printed numbers 
-    let count_total = 1_000_000_000;
-    let count_total_string = format!("{}", count_total);
+    let count_total: i32 = 1_000_000_000;
     let indent: String = repeat(" ").take(4).collect();
-    
-    // Run counting functions concurrently
-    let start_time = Instant::now();
-    
-    let rt = Runtime::new().unwrap();
-    rt.block_on(async {
-        let task = spawn(async move {count_by_1(&count_total, &count_total_string).await;
-    });
-        task.await.unwrap();
-    });
 
-    let elapsed_time = start_time.elapsed().as_secs();
+    // Run counting functions concurrently
+    let mut start_time: Instant = Instant::now();
+    let rt: Runtime = Runtime::new().unwrap();
+    let future = run_concurrently(&count_total);
+    rt.block_on(future);
+    let elapsed_concurrent_time: u64 = start_time.elapsed().as_secs();
+
+    // Run counting functions consecutively
+    start_time = Instant::now();
+    run_consecutively(&count_total);
+    let elapsed_consecutive_time: u64 = start_time.elapsed().as_secs();
 
     // Display results
     println!(
 "\nTotal time taken to run all counting functions:
-{0}Consecutively: (future feature)
-{0}Concurrently: {1} seconds",
-        indent, elapsed_time
+{0}Concurrently: {1} seconds
+{0}Consecutively: {2} seconds",
+        indent, elapsed_concurrent_time, elapsed_consecutive_time
     );
 }
 
-async fn count_by_1(&count_total: &i32, count_total_string: &String) {
-    println!("Counting by 1 to {}:", count_total_string);
-    for i in 1..=count_total {
-        // Print progress every time another 10% of the total is counted
-        if i % (count_total as f64 * 0.1) as i32 == 0 {
-            let percentage_complete = (i as f64 / count_total as f64) * 100.0;
-            println!("{}%: {} ", percentage_complete, i);
-        }
+async fn count_by_1(&count_total: &i32) -> () {
+    for _ in 1..=count_total {
+        continue;
     }
 }
 
-async fn count_by_fibonacci_nums(
-    &count_total: &i32,
-    count_total_string: &String
-) {
-    let mut i: i32 = 1;
+async fn count_by_2(&count_total: &i32) -> () {
+    let mut i = 2;
+
+    loop {
+        if i > count_total {
+            break;
+        }
+        i += 2;
+    }
+}
+
+async fn count_by_fibonacci_nums(&count_total: &i32) -> () {
     let mut current: i32 = 1;
     let mut prev: i32 = 0;
     
-    println!("\nCounting using fibonacci numbers from 1 to \
-        the closest number >= {}:", {count_total_string});
     loop {
-        // Print "prev" then compare to "count_total" before continuing to avoid
-        // unnecessary processing
-        println!("{}: {}", (i).to_string(), prev);
         if prev > count_total {
             break;
-        }
+        }    
 
         let next: i32 = prev + current;
 
         prev = current;
         current = next;
-        i += 1;
-    }
+    }    
+}    
+
+async fn run_concurrently(&count_total: &i32) {
+    println!(
+"\nConcurrently running the following counting functions:
+    count_by_1
+    count_by_2
+    count_by_fibonacci_nums");
+    join!(
+        count_by_1(&count_total),
+        count_by_2(&count_total),
+        count_by_fibonacci_nums(&count_total),
+    );
 }
 
-async fn double_from_1(&count_total: &i32, count_total_string: &String) {
-    let mut i = 1;
-
-    println!("\nDoubling from 1 to the closest number >= {}:", count_total_string);
-    loop {
-        println!("{} ", i);
-
-        if i > count_total {
-            break;
-        }
-        i *= 2;
-    }
+fn run_consecutively(&count_total: &i32) {
+    println!(
+"\nConcsecutively running the following counting functions:
+count_by_1
+count_by_2
+count_by_fibonacci_nums");
+        Runtime::new().unwrap().block_on(async {
+        count_by_1(&count_total).await;
+        count_by_2(&count_total).await;
+        count_by_fibonacci_nums(&count_total).await;
+    });
 }
