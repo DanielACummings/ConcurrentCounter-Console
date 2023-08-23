43fd6427f623af42698dd4c40267a708512c84ee
Daniel <danielac@protonmail.com>
2023-08-23 11:08:02 -0600

    Consolidate repetitive printing & minor touch up

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,24 @@ fn main() {
     // TODO: add commas to printed numbers 
     let count_total: i32 = 1_000_000_000;
     let indent: String = repeat(" ").take(4).collect();
+    let function_message: &str =
+"running the following counting functions:
+    count_by_1
+    count_by_2
+    count_by_fibonacci_nums";
 
     // Run counting functions concurrently
     let mut start_time: Instant = Instant::now();
     let rt: Runtime = Runtime::new().unwrap();
     let future = run_concurrently(&count_total);
+
+    println!("Concurrently {}", function_message);
     rt.block_on(future);
     let elapsed_concurrent_time: u64 = start_time.elapsed().as_secs();
 
     // Run counting functions consecutively
     start_time = Instant::now();
+    println!("Consecutively {}", function_message);
     run_consecutively(&count_total);
     let elapsed_consecutive_time: u64 = start_time.elapsed().as_secs();
 
@@ -53,21 +61,14 @@ async fn count_by_fibonacci_nums(&count_total: &i32) -> () {
     loop {
         if prev > count_total {
             break;
-        }    
-
+        }
         let next: i32 = prev + current;
-
         prev = current;
         current = next;
-    }    
-}    
+    }
+}
 
 async fn run_concurrently(&count_total: &i32) {
-    println!(
-"\nConcurrently running the following counting functions:
-    count_by_1
-    count_by_2
-    count_by_fibonacci_nums");
     join!(
         count_by_1(&count_total),
         count_by_2(&count_total),
@@ -76,12 +77,7 @@ async fn run_concurrently(&count_total: &i32) {
 }
 
 fn run_consecutively(&count_total: &i32) {
-    println!(
-"\nConcsecutively running the following counting functions:
-count_by_1
-count_by_2
-count_by_fibonacci_nums");
-        Runtime::new().unwrap().block_on(async {
+    Runtime::new().unwrap().block_on(async {
         count_by_1(&count_total).await;
         count_by_2(&count_total).await;
         count_by_fibonacci_nums(&count_total).await;
